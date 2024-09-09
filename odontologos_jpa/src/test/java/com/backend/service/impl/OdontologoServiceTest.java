56fd2a541e3233a3c5e4ea8c15917560465bb634
henry-cv <thechosenone.cc@gmail.com>
2024-09-09 17:16:06 -0500

    OdontologosTest mockito, con dos test

--- odontologos_jpa/src/test/java/com/backend/service/impl/OdontologoServiceTest.java
+++ odontologos_jpa/src/test/java/com/backend/service/impl/OdontologoServiceTest.java
@@ -71,40 +71,5 @@ class OdontologoServiceTest {
       odontologoService.listarOdontologos();
     assertFalse(listadoDeOdontologos.isEmpty());
   }
-  @Test @Order(1)
-  public void debeSerPosibleRegistrarNuevoOdontologo(){
-    OdontologoEntradaDto ingresaOdontologo = new OdontologoEntradaDto("mat-011", "Juliana", "Vera");
-    OdontologoSalidaDto odontologoRegistrado = odontologoService.registrarOdontologo(ingresaOdontologo);
-      System.out.println("SALIDA SYSTEM OUT: "+odontologoRegistrado);
-    assertNotNull(odontologoRegistrado.getId());
-  }
-  @Test @Order(3)
-  void seDebePoderListarTodosLosOdontologosAlmacenadosEnH2CuandoListaDistintaDeCero() {
-    List<OdontologoSalidaDto> listadoOdontologos = odontologoService.listarOdontologos();
-    assertNotEquals(0, listadoOdontologos.size());
-  }
 
-  @Test @Order(2)
-  void dadoUnIdExistenteSeDebeBuscarEnH2ElOdontologoCorrespondiente() {
-    OdontologoSalidaDto encontrado = odontologoService.buscarOdontologoPorId(2L);
-    assertNotNull(encontrado.getId());
-  }
-  @Test @Order(5)
-  void dadoUnIdExistenteSeDebeEliminarOdontologoCorrespondienteEnH2() throws
-    ResourceNotFoundException {
-    //En este caso elimina el último de la lista
-    Long id = odontologoService.listarOdontologos().get(odontologoService.listarOdontologos().size()-1).getId();
-    OdontologoSalidaDto encontrado = odontologoService.buscarOdontologoPorId(id);
-    System.err.println("Odontologo Borrado: " + encontrado);
-    odontologoService.eliminarOdontologo(id);
-    assertNotNull(encontrado);
-  }
-  @Test @Order(4)
-  void dadoUnIdExistenteYDatosSeDebeModificarOdontologoCorrespondienteEnH2() {
-    //En este caso modifica el último de la lista
-    Long id = odontologoService.listarOdontologos().get(odontologoService.listarOdontologos().size()-1).getId();
-    OdontologoEntradaDto actualizaOdontologo = new OdontologoEntradaDto("prueba-001","Antonio","Lamas");
-    OdontologoSalidaDto odontologoSalidaDto = odontologoService.actualizarOdontologo(actualizaOdontologo, id);
-    assertNotNull(odontologoSalidaDto);
-  }
 }
